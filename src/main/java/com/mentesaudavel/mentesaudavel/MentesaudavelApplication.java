6cf13c0119b726a6f7c11cd87c9d69302644526b
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2025-04-22 16:38:29 -0300

    initial commit

---
+++ src/main/java/com/mentesaudavel/mentesaudavel/MentesaudavelApplication.java
@@ -0,0 +1,13 @@
+package com.mentesaudavel.mentesaudavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentesaudavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentesaudavelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentesaudavel/mentesaudavel/MentesaudavelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentesaudavel.mentesaudavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentesaudavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
