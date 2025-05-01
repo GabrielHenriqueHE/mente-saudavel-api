735b4abdfe3b32b1889b0516f17c0c9bd094cac4
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2025-05-01 20:18:06 -0300

    chore: moving file to services/ package

--- src/main/java/com/mentesaudavel/mentesaudavel/core/security/implementations/UserDetailsServiceImpl.java
+++ src/main/java/com/mentesaudavel/mentesaudavel/core/security/implementations/UserDetailsServiceImpl.java
@@ -1,16 +1,15 @@
-package com.mentesaudavel.mentesaudavel.core.security.services;
+package com.mentesaudavel.mentesaudavel.core.security.implementations;
 
 import com.mentesaudavel.mentesaudavel.core.entities.User;
 import com.mentesaudavel.mentesaudavel.core.exceptions.ResourceNotFoundException;
 import com.mentesaudavel.mentesaudavel.core.exceptions.UnauthorizedAccessException;
-import com.mentesaudavel.mentesaudavel.core.security.implementations.UserDetailsImpl;
 import com.mentesaudavel.mentesaudavel.core.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -19,9 +18,19 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private UserService userService;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) {
         try {
             User user = this.userService.findUserByEmail(username);
+
+            return new UserDetailsImpl(user);
+        } catch (ResourceNotFoundException ex) {
+            throw new UnauthorizedAccessException(ex.getMessage());
+        }
+    }
+
+    public UserDetails loadUserById(UUID id) {
+        try {
+            User user = this.userService.findByUserId(id);
 
             return new UserDetailsImpl(user);
         } catch (ResourceNotFoundException ex) {
