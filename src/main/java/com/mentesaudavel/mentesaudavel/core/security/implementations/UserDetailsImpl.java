1648e8f46cd146ffd6bdc381fd25167b1c58c9ca
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2025-05-01 20:20:28 -0300

    feat: adding getter for user in UserDetailsImpl class

--- src/main/java/com/mentesaudavel/mentesaudavel/core/security/implementations/UserDetailsImpl.java
+++ src/main/java/com/mentesaudavel/mentesaudavel/core/security/implementations/UserDetailsImpl.java
@@ -19,6 +19,10 @@ public class UserDetailsImpl implements UserDetails {
         this.user = user;
     }
 
+    public User getUser() {
+        return this.user;
+    }
+
     @Override
     public String getPassword() {
         return this.user.getPassword();
