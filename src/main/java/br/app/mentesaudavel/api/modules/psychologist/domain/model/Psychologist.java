11f26b3028cd9de0b36bf165641b3f0d90d47276
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2025-05-23 23:33:04 -0300

    FEAT (psychologist): add psychologist entity

---
+++ src/main/java/br/app/mentesaudavel/api/modules/psychologist/domain/model/Psychologist.java
@@ -0,0 +1,78 @@
+package br.app.mentesaudavel.api.modules.psychologist.domain.model;
+
+import br.app.mentesaudavel.api.modules.user.domain.model.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_psychologists")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Psychologist implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "crp", nullable = false, unique = true, length = 8)
+    private String crp;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "activities_start_date", nullable = false)
+    private LocalDate activitiesStartDate;
+
+    @Column(name = "about", length = 2600)
+    private String about;
+
+    @Column(name = "slug", unique = true, length = 50)
+    private String slug;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public Psychologist(
+            String name,
+            String crp,
+            LocalDate birthDate,
+            LocalDate activitiesStartDate,
+            String slug,
+            User user
+    ) {
+        this.name = name;
+        this.crp = crp;
+        this.birthDate = birthDate;
+        this.activitiesStartDate = activitiesStartDate;
+        this.slug = slug;
+        this.user = user;
+    }
+}
